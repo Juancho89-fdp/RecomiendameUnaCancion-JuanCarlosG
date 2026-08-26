114791b09c663b6f2d6d2e626267d57775c0c02a
Juan Carlos Guerrero <j478431@hotmail.com>
2026-08-26 11:39:55 -0600

    Se agregó primera recomendacion musical

---
+++ RecomiendameUnaCancion.java
@@ -0,0 +1,18 @@
+/*
+ * Autor: Juan Carlos Guerrero
+ * Asignatura: Modelos de Programación Orientada a Objetos
+ *
+ * Descripción:
+ * Programa colaborativo para compartir recomendaciones musicales.
+ */
+
+public class RecomiendameUnaCancion {
+    
+    public static void main(String[] args) {
+        System.out.println(" RECOMIÉNDAME UNA CANCIÓN \n");
+        System.out.println("Juan Carlos recomienda:");
+        System.out.println("Canción: We Are The People");
+        System.out.println("Artista: Empire Of The Sun");
+        System.out.println("¿Por qué?: Porque es una cancion nostalgica que habla sobre gobernar nuestra propia vida");
+    }
+}
